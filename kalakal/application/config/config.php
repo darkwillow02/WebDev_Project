93c5596057b0410fa4a1929a54f957244e32e615
USER <nicail180@gmail.com>
2018-01-26 11:22:32 +0800

    Renaming WebDev

--- kalakal/application/config/config.php
+++ kalakal/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/WebDev_Project/kalakal/';
+$config['base_url'] = 'http://localhost/webdev/kalakal/';
 
 /*
 |--------------------------------------------------------------------------
